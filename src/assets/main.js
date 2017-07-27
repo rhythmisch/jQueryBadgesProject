31a552a8ae6b20ae5d14f7e4df524dd541c771c9
rhythmisch <nekater@gmx.de>
2017-07-27 17:36:47 +0200

    2nd try

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,9 +4,17 @@ $.ajax({
   url: 'https://www.codeschool.com/users/aaubele.json',
   dataType: 'jsonp',
   success: function(response){
+    $.each(response.course.completed, function(i, courseBadge){
+      var badge = $('<div class="course"></div>');
+      var title = courseBadge.tile;
+      var image = courseBadge.url;
+      var dong = courseBadge.badge;
+      $('#badges').append(badge);
+      $('.course').html('<h3>'+title+'</h3><img src="'+dong+'" /><a href="'+image+'" target="_blank" class="btn btn-primary">See Course</a>');
+    });
 
 
-    
+
   }
 });
 
