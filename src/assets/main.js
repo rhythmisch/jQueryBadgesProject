05aef9731791949d06d29facb5639117cb422190
rhythmisch <nekater@gmx.de>
2017-07-27 17:10:03 +0200

    begin ajax request

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,12 @@
 $(function() {
 
-  // your code will go here
+$.ajax({
+  url: 'https://www.codeschool.com/users/aaubele.json',
+  dataType: 'jsonp',
+  success: function(response){
+    console.log("Success!");
+  }
+});
+
 
 });
